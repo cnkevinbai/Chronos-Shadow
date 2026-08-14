f424e555506d5bf5424bb37fba42b291df47c76a
kevinhei <kevinhei@gitcode.com>
2026-08-14 09:33:30 +0800

    chore: add cargo check + rust-only release build helpers
    - check.bat: cargo check --release (fast typecheck)
    
    - build-rust.bat: cargo build --release (rust-only, no frontend/package)

---
+++ build-rust.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d D:\Chronos-Shadow\chronos-shadow
+set CARGO_HOME=D:\rust\.cargo
+set CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
+set PATH=D:\rust\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin;D:\rust\.cargo\bin;C:\Program Files\nodejs;C:\WINDOWS\system32;C:\WINDOWS
+set CARGO_BUILD_JOBS=1
+D:\rust\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin\cargo.exe build --release --manifest-path src-tauri/Cargo.toml
